3f5fa5691db7353404bcd60392a8aed80bd3094d
Ian McLerran <imclerran@protonmail.com>
2024-06-04 15:12:22 -0500

    Tutput of `roc test` doesn't appear in pipeline output. Test to see if changing order shows the output.

--- ci/all_tests.sh
+++ ci/all_tests.sh
@@ -8,11 +8,11 @@ roc='./roc_nightly/roc'
 
 src_dir='./src'
 
+# roc test
+$roc test $src_dir/test-stub.roc
+
 # roc check
 $roc check $src_dir/main.roc
 
 # roc build
 $roc build $src_dir/main.roc --linker=legacy
-
-# roc test
-$roc test $src_dir/test-stub.roc
